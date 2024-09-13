fef4960b3fe1f9d6550454fb8c6d2f5e3fa8f3bb
Chi Dung <chidung091@gmail.com>
2024-09-13 22:00:37 +0700

    Refactor nginx configuration and Dockerfile
    - Removed unnecessary code from convert-nginx.sh
    - Updated nginx.conf to include proxy_pass to Next.js app
    - Deleted unused Dockerfile for nginx
    - Updated docker-compose.yml to use nginx:latest image
    - Removed backup.conf and my-nginx.conf from nginx-docker directory

--- nginx-docker/convert-nginx.sh
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/env bash
-
-envsubst '$${NEXTJS_CONTAINER_IP}' < /etc/nginx/conf.d/my-nginx.conf > /etc/nginx/conf.d/default.conf 
-rm -rf /etc/nginx/conf.d/my-nginx.conf
-nginx -g "daemon off;"
